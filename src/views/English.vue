0e46106a6601f00da535049a3a3d9d03d05716f2
robartozh29 <robertozh29@hotmail.com>
2024-03-01 17:44:00 -0600

    views english and home init

---
+++ src/views/English.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="English">
+    Hola mundo
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import Navbar from '../components/Navbar.vue'
+
+export default {
+  name: 'English',
+  data(){
+    return{
+      translation:{
+        navbar:{
+          sections: ["Inicio", "Sobre mi", "Proyectos", "Contacto"]
+        },
+        header:{
+          profession: "Ingeniero en Sistemas",
+          phrase: "Programador Web especializado en construir aplicaciones front-end.",
+          resume: 'Curriculum',
+          resumeLink: 'cv_esp.pdf'
+        },
+        about:{
+          description:`Mi nombre es Roberto, soy un programador de software con una gran pasion por la tecnologia
+                y su aplicabilidad. Desde que soy niño he soñado con la tecnologia que nosotros como humanos estaremos
+                desarrollando en un futuro no muy lejano. Siendo ya un adulto,  mi sueños es contribuir y aprender 
+                lo maximo posible de las tecnologias modernas y futuras`,
+          experience: `Actualmente trabajo desarrollando web, front-end y back-end para una agencia de marketing, al igual que 
+          algunos proyectos independientes. Trabajo con diferentes tecnologias y frameworks tales como:`
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.spanish{
+  width: 100%;
+  min-height: 100vh;
+  background-color: #171717aa;
+}
+
+</style>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="home">
+        <Navbar 
+            :sections="info.navbar.sections"
+        />
+    </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+
+export default {
+    name: 'Home',
+    data () {
+      return {}
+    }
+}
+
+</script>
+
+<style>
+/*----- HOME CONTAINER ----- */
+.home{
+  width: 100%;
+  min-height: 100vh;
+  background-color: #171717;
+  text-align: left;
+}
+</style>
