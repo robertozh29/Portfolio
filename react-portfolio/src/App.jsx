9d0df8515254cfbdd47407e01a5d145059adffa3
Roberto Zepeda <robertozh29@hotmail.com>
2025-02-02 17:41:55 -0600

    Init Home page

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -2,13 +2,14 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import Example from './pages/Example'
+import Home from './pages/Home'
 import './App.css'
 
 function App() {
   return (
       <Router>
         <Routes>
-          <Route path="/" element={<Example title="Home"/>} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<Example title="About"/>} />
           <Route path="/projects" element={<Example title="Projects"/>} />
         </Routes>
